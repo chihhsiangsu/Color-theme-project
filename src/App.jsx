155989a052a323eb176c05ca3a6d282648894cf9
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-04-02 12:56:21 +0200

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import { ColorForm } from "./Components/Color/ColorForm/ColorForm.jsx";
+import { uid } from "uid";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleAddColor(newColor) {
+    setColors([{ id: uid(), ...newColor }, ...colors]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
-      <ColorForm />
-      {initialColors.map((color) => {
+      <ColorForm onSubmitColor={handleAddColor} />
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
