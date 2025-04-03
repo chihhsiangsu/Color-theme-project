e5eeede45f7dd42cac5db3e0c1875c68916797ee
Chih-Hsiang Su <su.chihhsiang@icloud.com>
2025-04-03 17:55:28 +0200

    delete console.log

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -32,7 +32,6 @@ export default function Color({ color, onDelete, onUpdate }) {
         }
       );
       const data = await response.json();
-      console.log("✅ Contrast API result:", data);
       setContrastResult(data);
     }
 
